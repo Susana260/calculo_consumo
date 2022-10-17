547caf3e9cfff5a992d05a27bc44144628327fc5
Susana Borges dos Santos <susana.santoa@unidavi.edu.br>
2022-10-17 17:11:22 -0300

    select inserido

--- resources/views/consumo.blade.php
+++ resources/views/consumo.blade.php
@@ -15,10 +15,6 @@
                 <p>Esta aplicação tem como finalidade demonstrar os valores que
                     serão gastos com combustível durante uma viagem, com base no
                 consumo do seu veículo, e com a distância determinada por você</p>
-                <p>Os combustíveis disponíveis para este cálculo são:</p>
-                <ul>
-                    <li><b>Gasolina - R$ 4.80</b></li>
-                </ul>
             </div>
         </div>
         
@@ -26,12 +22,22 @@
             <h2>Cálculo do valor (R$) do consumo</h2>
             <div class="conteudo-painel">
                 <form action="{{url('/gasto')}}" method="get">
+                    <label for="combustivel">Combustível</label>
+                    <select name="combustivel">
+                        <option value="Gasolina">Gasolina</option>
+                        <option value="Etanol">Etanol</option>
+                        <option value="Diesel">Diesel</option>
+                    </select>
+                    <br>
+                    <label for="valorcombustivel">Valor</label>
+                    <input type="number" name="valorcombustivel" step="0.1" required/>
+                    <br>
                     <label for="distancia">Distancia em Km a ser percorrida</label>
                     <input type="number" class="campoTexto" name="distancia" required />
-
+                    <br>
                     <label for="autonomia">Consumo de combustível do veículo (Km/L)</label>
-                    <input type="number" class="campoTexto" name="autonomia" required/>
-
+                    <input type="number" class="campoTexto" name="autonomia" step="0.01" required/>
+                    <br>
                     <button class="botao" type="submit">Calcular</button>
                 </form>
             </div>
